4417bb87975a9ce0e9cd9ca1a79f084e228c6dd1
Leonardo Matos <120342838+LeoCMatos@users.noreply.github.com>
2023-09-19 13:04:37 -0300

    update fundo logo

--- hamburgueria/src/app/components/header/header.component.ts
+++ hamburgueria/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Navigation, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  isHomePage = false
+  isMenuPage = false
+
+  constructor(private router: Router){}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd){
+        this.isHomePage = event.url === '/cardapio'
+        this.isMenuPage = event.url === '/menu'
+      }
+    })
+  }
 
 }
